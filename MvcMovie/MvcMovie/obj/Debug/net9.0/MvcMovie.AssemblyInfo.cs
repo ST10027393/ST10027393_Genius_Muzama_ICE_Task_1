2fd619d9bd1739b6a563826d43a9060890bb9a03
Genius Muzama <117445671+ST10027393@users.noreply.github.com>
2025-02-21 09:47:56 +0200

    7th Commit
    In this commit I added some search and filter functionality to the movie list table , mainly the ability to filter by genre and search by movie name

--- MvcMovie/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeb4d4f7655ef191a85b19af0d668a8b65715a00")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
